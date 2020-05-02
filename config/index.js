967adb16d2d45b43e031ef95506e1142415e2c27
bradford-james <bwettig@gmail.com>
2020-05-02 08:00:48 -0500

    Added production env vars

--- config/index.js
+++ config/index.js
@@ -1,5 +1,6 @@
 exports.corsOptions = {
-  origin: process.env.PRODUCTION_CORS_URL || 'http://localhost:8081',
+  // origin: process.env.PRODUCTION_CORS_URL || 'http://localhost:8081',
+  origin: 'https://nostalgic-pasteur-f4a6fb.netlify.app',
 };
 
 exports.db_url =
